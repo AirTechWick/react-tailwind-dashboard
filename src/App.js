706079e34dfd106a4295737aa494cfd6b5270873
Erik <erikbolt11@gmail.com>
2024-01-20 11:51:30 -0800

    Fixed build

--- src/App.js
+++ src/App.js
@@ -13,32 +13,6 @@ function App() {
     </Router>
   );
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p class="font-bold" >
-  //         Edit <code>src/App.js</code> and save to be awesome.
-  //       </p>
-  //       <Link
-  //         to="/dashboard"
-  //         className="App-link"
-  //       >
-  //         Go to Dashboard
-  //       </Link>
-
-
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Go to Dashboard
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
